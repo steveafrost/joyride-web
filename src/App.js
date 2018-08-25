fede887630808028403eeec7b3088dbe1fbdda38
Steve Frost <sfrost@nerdery.com>
2018-08-25 16:28:22 -0500

    react router setup & scaffolding

--- src/App.js
+++ src/App.js
@@ -1,21 +1,36 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import styled from 'styled-components';
+import Home from './pages/Home';
+import Contact from './pages/Contact';
+import About from './pages/About';
+
+const AppContainer = styled.div``;
+
+const Header = styled.header``;
+
+const Main = styled.main``;
+
+const Footer = styled.footer``;
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <Router>
+                <AppContainer>
+                    <Header />
+                    <Main>
+                        <div>
+                            <Route exact path="/" component={Home} />
+                            <Route path="/about" component={About} />
+                            <Route path="/contact" component={Contact} />
+                        </div>
+                    </Main>
+                    <Footer />
+                </AppContainer>
+            </Router>
+        );
+    }
 }
 
 export default App;
